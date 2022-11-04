1360f3e9d343ad3f87dd063457ca50bcb5aed4b9
Dontcode44 <kabutto378@gmail.com>
2022-11-04 12:38:48 -0500

    [ADD] - New structure from the users.entity

--- src/users/entities/user.entity.ts
+++ src/users/entities/user.entity.ts
@@ -1,17 +1,22 @@
+import { IsNotEmpty } from 'class-validator';
 import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
 
 @Entity({ name: 'users' })
 /* We have a User class with a username, email, and password. The username and email are unique */
 export class User {
   @PrimaryGeneratedColumn()
+  @IsNotEmpty()
   id: number;
 
+  @IsNotEmpty()
   @Column({ unique: true })
   username: string;
 
+  @IsNotEmpty()
   @Column({ unique: true })
   email: string;
 
+  @IsNotEmpty()
   @Column()
   password: string;
 }
