bbd344732627577faba30f5f473306d821d1749e
Dontcode44 <kabutto378@gmail.com>
2022-11-05 15:12:26 -0500

    [ADD] - Propierties required in the schemas

--- src/users/entities/dto/create-user.dto.ts
+++ src/users/entities/dto/create-user.dto.ts
@@ -1,15 +1,19 @@
 import { IsEmail, IsNotEmpty, IsString } from 'class-validator';
+import { ApiProperty } from '@nestjs/swagger';
 
 export class CreateUserDto {
+  @ApiProperty()
   @IsNotEmpty()
   @IsString()
   username: string;
 
+  @ApiProperty()
   @IsNotEmpty()
   @IsEmail()
   @IsString()
   email: string;
 
+  @ApiProperty()
   @IsNotEmpty()
   @IsString()
   password: string;
--- src/users/entities/dto/update-user.dto.ts
+++ src/users/entities/dto/update-user.dto.ts
@@ -1,15 +1,19 @@
 import { IsEmail, IsNotEmpty, IsString } from 'class-validator';
+import { ApiProperty } from '@nestjs/swagger';
 
 export class UpdateUserDto {
+  @ApiProperty()
   @IsString()
   @IsNotEmpty()
   username?: string;
 
+  @ApiProperty()
   @IsNotEmpty()
   @IsString()
   @IsEmail()
   email?: string;
 
+  @ApiProperty()
   @IsNotEmpty()
   @IsString()
   password?: string;
